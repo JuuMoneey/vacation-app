79f98957798cb5b357cd4f081b1174d0c1115743
Asadur Rahman <asadrm63@gmail.com>
2023-06-27 09:15:02 -0400

    created components Asad

---
+++ src/components/attractions/Attractions.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Attractions = () => {
+  return (
+    <div>
+    <h3>Logding</h3>	
+    <h3>Beaches</h3>	
+    <h3>Resturants</h3>	
+    <h3>Activities</h3>	
+    <h3>Location</h3>	
+    <h3>Info</h3>	
+    <p>weather</p>
+    <p>Average Cost</p>
+	<div className='Comments'>
+    <h3>Comments</h3>
+    </div>
+    </div>
+  )
+}
+
+export default Attractions
