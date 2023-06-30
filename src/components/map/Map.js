66cf9cff887752d891c5abbd97024175ab938657
JBelgrave <JudahBelgrave@gmail.com>
2023-06-30 11:53:13 -0400

    added styling back to respective css files, added world map to home page

---
+++ src/components/map/Map.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+import "./Map.css";
+
+function Map() {
+  return (
+    <div>
+      <ComposableMap>
+        <Geographies geography="/features.json">
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
+  )
+};
+
+export default Map
