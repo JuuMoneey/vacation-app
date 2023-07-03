2107ff2800f567562ea7cb81a6b81666135fad1b
Asadur Rahman <asadrm63@gmail.com>
2023-07-02 23:47:55 -0400

    attractions and addPastTrip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { gapi } from 'gapi-script'
 import './App.css';
+import NavBar from './components/navBar/navBar'
+import Login from './components/login/Login';
+import Logout from './components/logout/logout'
+import { Routes, Route } from "react-router-dom";
+import Home from './components/home/Home';
+import Locations from './components/locations/Locations';
+
+const clientId = '334215639628-vu09cfq9ob860n6hj48vosfsdl545reo.apps.googleusercontent.com';
 
 function App() {
+  const [userProfile, setUserProfile] = useState(null)
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({ 
+        clientId: clientId,
+        scope: ''
+      })
+    };
+    gapi.load('client:auth2', start)
+  });
+
+
+if(userProfile){
+  return (
+  <div className="Vacation-App">
+  {/* <NavBar/> */}
+  <div className='container'>
+  <h2>Welcome {userProfile.givenName}!</h2>
+    <Logout userProfile={userProfile} setUserProfile={setUserProfile}/>
+        <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/locations" element={<Locations />} />
+      </Routes>
+  </div>
+ </div>
+ );
+}else{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Login userProfile={userProfile} setUserProfile={setUserProfile}/>
     </div>
   );
+  }
 }
 
 export default App;
